79d28eb43af34615cc16ebc9dc2337d69bb81232
cmeyrueix4 <cmeyrueix44@gmail.com>
2020-08-12 14:20:12 -0700

    Finished implementing Sequelize version of app including cart and order functionality, will now start a NoSQL version of this!

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -39,13 +41,16 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 
-//Create association between Product, User, Cart, and CartItem
+//Create association between all models
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
 
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -23,8 +23,8 @@ router.post('/cart', shopController.postCart);
 
 router.post('/cart-delete-item', shopController.deleteCartItem);
 
-router.get('/checkout', shopController.getCheckout);
+router.post('/create-order', shopController.postOrder);
 
-router.get('/orders', shopController.getOrders)
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
