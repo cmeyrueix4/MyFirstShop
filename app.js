7f191cf82200eb8e912cdd41e40e8aece935caa0
cmeyrueix4 <cmeyrueix44@gmail.com>
2020-08-25 17:22:25 -0700

    Attempted to create a session but not fully implemented

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 // const expressHbs = require('express-handlebars')
 
-const app = express();
+const app = express(); 
+// const store = new MongoDBStore({
+//     url: 'mongodb+srv://store-manager:manaGEME173@cluster0.zurim.mongodb.net/shop?authSource=admin',
+//     collection: 'sessions'
+// });
 
 
 // app.engine('hbs', expressHbs({layoutDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
@@ -21,7 +27,7 @@ const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-
+// app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
 
 app.use((req, res, next) => {
     User.findById('5f405caf7481f32ba8a45daa')
@@ -37,7 +43,7 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('')
+mongoose.connect('mongodb://store-manager:manaGEME173@cluster0-shard-00-00.zurim.mongodb.net:27017,cluster0-shard-00-01.zurim.mongodb.net:27017,cluster0-shard-00-02.zurim.mongodb.net:27017/shop?ssl=true&replicaSet=atlas-96hs71-shard-0&authSource=admin&w=majority')
 .then(result => {
     User.findOne().then(user => {
         if(!user){
