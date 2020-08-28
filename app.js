e2adb7219ae05b5bb4e592207c483539cb1ff302
cmeyrueix4 <cmeyrueix44@gmail.com>
2020-08-28 14:19:05 -0700

    added csrf protection and flash messages for failed authentication

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 // const expressHbs = require('express-handlebars')
 
 const MONGODB = ''
 
 const app = express(); 
+
 const store = new MongoDBStore({
     uri: MONGODB,
     collection: 'sessions'
 });
 
+const csrfProtection = csrf();
 
 // app.engine('hbs', expressHbs({layoutDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
 app.set('view engine', 'ejs');
@@ -36,6 +39,9 @@ app.use(session({
     store: store
 }));
 
+app.use(csrfProtection);
+app.use(flash());
+
 app.use((req, res, next) => {
     if(!req.session.user){
         return next();
@@ -45,6 +51,12 @@ app.use((req, res, next) => {
         req.user = user;
         next();
     }).catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
 });
 
 app.use('/admin/', adminRoutes);
